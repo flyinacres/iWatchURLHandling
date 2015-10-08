d8a40675cc2ead37efbb0ae8c252b8fde4f5959e
Ronald Fischer <rff@ronalds-mbp-2.attlocal.net>
2015-10-08 16:28:22 -0700

    First pass on this iWatch task.

---
+++ Downloading Web Content WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,40 @@
+//
+//  InterfaceController.swift
+//  Downloading Web Content WatchKit Extension
+//
+//  Created by Ronald Fischer on 10/8/15.
+//  Copyright (c) 2015 qpiapps. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+            if error == nil {
+                println(data)
+            } else {
+                println(error)
+            }
+        })
+        
+        task.resume()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
